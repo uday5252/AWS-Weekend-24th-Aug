9d682c27bc9c708c17f9585686551b626816ca53
uday <udaykirantutor@gmail.com>
2025-08-31 21:20:19 +0530

    project

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://uday:Welcome%401234@books-store-mern.csxtwvw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=books-store-mern';
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
